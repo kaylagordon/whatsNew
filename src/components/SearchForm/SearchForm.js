fd2138b7467f91ffea6ebd615ebc05b8b366cf3b
Kayla Wood <kaylaewood@gmail.com>
2019-11-18 21:32:53 -0700

    Add skeleton for search bar

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,7 +4,9 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <div>
-      <p>This is the search bar.</p>
+      <h1>What's <span className='heading-highlight'>New?</span></h1>
+      <input type='textBox' placeholder='Search for news articles here.' />
+      <button>Search Now</button>
     </div>
   )
 }
