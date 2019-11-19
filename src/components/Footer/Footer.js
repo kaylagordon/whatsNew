14c8b3f782e35356d75929fd7e461b0a9253e1f7
Kayla Wood <kaylaewood@gmail.com>
2019-11-19 15:54:52 -0700

    Add footer to credit icons

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      Icons from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </div>
+  )
+}
+
+export default Footer;
