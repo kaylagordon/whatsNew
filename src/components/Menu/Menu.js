8632bc18a2bb21f3e1cfae6f8e17928d9f5cd383
Kayla Wood <kaylaewood@gmail.com>
2019-11-19 15:43:33 -0700

    Add styling to menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './Menu.css'
+import placeholder from '../../images/placeholder.svg'
+import flask from '../../images/flask.svg'
+import heart from '../../images/heart.svg'
+import projectManagement from '../../images/project-management.svg'
+import camera from '../../images/video-camera.svg'
 
 function Menu(props) {
   return (
     <div className='menu'>
-      <button onClick={() => props.changeNewsSource(props.local)}>Local News</button>
-      <button onClick={() => props.changeNewsSource(props.technology)}>Technology</button>
-      <button onClick={() => props.changeNewsSource(props.entertainment)}>Entertainment</button>
-      <button onClick={() => props.changeNewsSource(props.science)}>Science</button>
-      <button onClick={() => props.changeNewsSource(props.health)}>Health</button>
+      <button className='menu-button' onClick={() => props.changeNewsSource(props.local)}>
+        <img className='icon' src={placeholder}/>
+        LOCAL NEWS
+      </button>
+      <button className='menu-button' onClick={() => props.changeNewsSource(props.technology)}>
+        <img className='icon' src={projectManagement}/>
+        TECHNOLOGY
+      </button>
+      <button className='menu-button' onClick={() => props.changeNewsSource(props.entertainment)}>
+        <img className='icon' src={camera}/>
+        ENTERTAINMENT
+      </button>
+      <button className='menu-button' onClick={() => props.changeNewsSource(props.science)}>
+        <img className='icon' src={flask}/>
+        SCIENCE
+      </button>
+      <button className='menu-button' onClick={() => props.changeNewsSource(props.health)}>
+        <img className='icon' src={heart}/>
+        HEALTH
+      </button>
     </div>
   )
 }
