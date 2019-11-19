a7b036837a0356c75edd1daf8944c6598f9d2bb3
Kayla Wood <kaylaewood@gmail.com>
2019-11-19 14:50:27 -0700

    Add styling to news container

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -5,9 +5,13 @@ function NewsArticle(props) {
   return (
     <div className='newsArticle'>
       <img className='newsImage' src={props.image} alt='{props.headline}' />
-      <h2>{props.headline}</h2>
-      <p>{props.description}</p>
-      <a href={props.link}>Link to article</a>
+      <div className='newsInfo'>
+        <h2>{props.headline}</h2>
+        <p>{props.description}</p>
+      </div>
+      <div className='link-box'>
+        <a href={props.link}>CLICK TO GO TO ARTICLE</a>
+      </div>
     </div>
   )
 }
