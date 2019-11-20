142e14fc8a1b9a071364b4724f350dc8d2e53535
Kayla Wood <kaylaewood@gmail.com>
2019-11-19 20:09:58 -0700

    Remove all data files no longer needed due to fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-// import local from '../../data/local';
-// import entertainment from '../../data/entertainment';
-// import health from '../../data/health';
-// import science from '../../data/science';
-// import technology from '../../data/technology';
 import './App.css';
 import SearchForm from '../SearchForm/SearchForm'
 import Menu from '../Menu/Menu'
