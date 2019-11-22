9e43184b61b052d02c21a99792bebdc6d0e42649
Kayla Wood <kaylaewood@gmail.com>
2019-11-22 16:16:17 -0700

    Change SearchForm to stateful component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './SearchForm.css';
 
-function SearchForm({searchNews}) {
+export default class SearchForm extends Component {
+constructor() {
+  super();
+  this.state = {
+    searchQuery: ''
+  }
+}
+
+submitSearch = event => {
+  event.preventDefault();
+  const { searchNews } = this.props;
+  searchNews(this.state.searchQuery);
+  this.resetInputs();
+}
+
+resetInputs = () => {
+  this.setState({
+    searchQuery = ''
+  })
+}
+
+updateSearchQuery = event => {
+  this.state({
+    searchQuery: event.target.value
+  })
+}
+
+render {
   return (
     <header>
       <h1>what's <span className='heading-highlight'>new</span> in your world?</h1>
-      <div className='search-section'>
-        <input id='search-input' type='textBox' placeholder='Search for news articles here' />
-        <button className='search-button' onClick={() => searchNews(document.getElementById('search-input').value)}>SEARCH</button>
-      </div>
+      <form className='search-section'>
+        <input id='search-input' type='textBox' placeholder='Search for news articles here' onChange={this.updateSearchQuery}/>
+        <button className='search-button' onClick={this.submitSearch}>SEARCH</button>
+      </form>
     </header>
   )
 }
